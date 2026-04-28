0014f6bf5eb57deabb1779e3a613c283ea75e086
Surya Janardhan <chintalajanardhan2004@gmail.com>
2026-04-28 09:37:39 +0530

    Configure GitHub Pages deployment action

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,12 @@
+const isGithubActions = process.env.GITHUB_ACTIONS || false;
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  output: 'export',
+  basePath: isGithubActions ? '/AI-Portfolio-Task' : '',
+  images: {
+    unoptimized: true,
+  },
+};
 
 export default nextConfig;
